c41e2506d877eafd11690b3bd5fbae2b7da30926
Stefanie Peschel <stefpeschel@gmail.com>
2022-11-16 09:45:24 +0100

    feat: add goodness-of-fit tests

--- R/gpdCvm_adapt.R
+++ R/gpdCvm_adapt.R
@@ -1,73 +1,70 @@
-gpdAd_adapt <- function(data, scale, shape, bootstrap = FALSE, bootnum = NULL,
-                        allowParallel = FALSE, numCores = 1){
-
-  if (bootstrap == TRUE & is.null(bootnum))
+gpdCvm_adapt <-  function(data, scale, shape, bootstrap = FALSE, bootnum = NULL, 
+                          allowParallel = FALSE, numCores = 1) 
+{
+  if (bootstrap == TRUE & is.null(bootnum)) 
     stop("Must specify some number of boostrap samples")
   n <- length(data)
-  # fit <- tryCatch(eva::gpdFit(data, nextremes = n, method = "mle"),
+  # fit <- tryCatch(gpdFit(data, nextremes = n, method = "mle"), 
   #                 error = function(w) {
   #                   return(NULL)
   #                 }, warning = function(w) {
   #                   return(NULL)
   #                 })
-  # if (is.null(fit))
+  # if (is.null(fit)) 
   #   stop("Maximum likelihood failed to converge at initial step")
   # scale <- fit$par.ests[1]
   # shape <- fit$par.ests[2]
   theta <- c(scale, shape)
-  if (bootstrap == FALSE & shape > 1)
+  if (bootstrap == FALSE & shape > 1) 
     stop("Estimated parameters are outside the table range, please use the bootstrap version")
   thresh <- eva:::findthresh(data, n)
   newdata <- eva::pgpd(data, loc = thresh, scale = scale, shape = shape)
   newdata <- sort(newdata)
-
-  # edit by SP:
-  n <- length(newdata)
-
   i <- seq(1, n, 1)
-  stat <- -n - (1/n) * sum((2 * i - 1) * (log(newdata) + log1p(-rev(newdata))))
+  stat <- sum((newdata - (2 * i - 1)/(2 * n))^2) + (1/(12 * 
+                                                         n))
   if (bootstrap == TRUE) {
     if (allowParallel == TRUE) {
       cl <- makeCluster(numCores)
       fun <- function(cl) {
         parSapply(cl, 1:bootnum, function(i, ...) {
-          gpdAdGen(n, theta)
+          gpdCvmGen(n, theta)
         })
       }
       teststat <- fun(cl)
       stopCluster(cl)
     }
     else {
-      teststat <- replicate(bootnum, gpdAdGen(n, theta))
+      teststat <- replicate(bootnum, gpdCvmGen(n, theta))
     }
     teststat <- teststat[!is.na(teststat)]
     eff <- length(teststat)
     p <- (sum(teststat > stat) + 1)/(eff + 2)
   }
   else {
-    row <- which(rownames(eva:::ADQuantiles) == max(round(shape,
-                                                    2), -0.5))
-    if (stat > eva:::ADQuantiles[row, 999]) {
-      pvals <- -log(as.numeric(colnames(eva:::ADQuantiles[950:999])))
-      x <- as.numeric(eva:::ADQuantiles[row, 950:999])
+    row <- which(rownames(eva:::CVMQuantiles) == max(round(shape, 
+                                                     2), -0.5))
+    if (stat > eva:::CVMQuantiles[row, 999]) {
+      pvals <- -log(as.numeric(colnames(eva:::CVMQuantiles[950:999])))
+      x <- as.numeric(eva:::CVMQuantiles[row, 950:999])
       y <- lm(pvals ~ x)
       stat <- as.data.frame(stat)
       colnames(stat) <- c("x")
       p <- as.numeric(exp(-predict(y, stat)))
     }
     else {
-      bound <- as.numeric(colnames(eva:::ADQuantiles)[which.max(stat <
-                                                            eva:::ADQuantiles[row, ])])
+      bound <- as.numeric(colnames(eva:::CVMQuantiles)[which.max(stat < 
+                                                             eva:::CVMQuantiles[row, ])])
       if (bound == 0.999) {
         p <- 0.999
       }
       else {
-        lower <- eva:::ADQuantiles[row, which(colnames(eva:::ADQuantiles) ==
-                                          bound + 0.001)]
-        upper <- eva:::ADQuantiles[row, which(colnames(eva:::ADQuantiles) ==
-                                          bound)]
+        lower <- eva:::CVMQuantiles[row, which(colnames(eva:::CVMQuantiles) == 
+                                           bound + 0.001)]
+        upper <- eva:::CVMQuantiles[row, which(colnames(eva:::CVMQuantiles) == 
+                                           bound)]
         dif <- (upper - stat)/(upper - lower)
-        val <- (dif * (-log(bound) - -log(bound + 0.001))) +
+        val <- (dif * (-log(bound) - -log(bound + 0.001))) + 
           log(bound)
         p <- exp(val)
       }
@@ -79,7 +76,7 @@ gpdAd_adapt <- function(data, scale, shape, bootstrap = FALSE, bootnum = NULL,
     names(out) <- c("statistic", "p.value", "theta")
   }
   else {
-    out <- list(as.numeric(stat), as.numeric(p), theta,
+    out <- list(as.numeric(stat), as.numeric(p), theta, 
                 eff)
     names(out) <- c("statistic", "p.value", "theta", "effective_bootnum")
   }
